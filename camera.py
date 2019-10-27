5fd11bdc4d06d1ce48a7ebc223ffd794d1b6efa2
Jiangxuan Chen <jxchen98@gmail.com>
2019-10-27 09:01:02 +0000

    Bug fixes

--- camera.py
+++ camera.py
@@ -8,24 +8,24 @@ from subprocess import call
 from google.cloud import storage
 
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.getcwd()+"/apiKeys.json"
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.getcwd()+"/APIKeys.json"
 
 
 url = "https://alpr-pdc653u2vq-ew.a.run.app/anpr"
 storage_client = storage.Client()
-
+imagePath = os.getcwd()+"/newImage.jpg"
 
 while True:
     call(["fswebcam", "-r 1280x720", "newImage.jpg"]) #takes image w/USB webcam
-    with open(os.getcwd()+"/newImage.jpg", "rb") as image: #uploads this to api endpoint as a POST request
+    with open(imagePath, "rb") as image: #uploads this to api endpoint as a POST request
         payload = {'file': image}
         result = requests.post(url, files = payload)
         print(result.text)
 	#upload to GCP bucket
-	storage_client = storage.Client()
-	bucket = storage_client.get_bucket('carvision-images')
-	blob = bucket.blob('newImage')
-	blob.upload_from_filename(image)
+        storage_client = storage.Client()
+        bucket = storage_client.get_bucket('carvision-images')
+        blob = bucket.blob('newImage')
+        blob.upload_from_filename(imagePath)
         sleep(3)
 
 
